b6d32208dbc1f511f94ea0ab406216ac17e11d5b
Austin <austinofficial007@gmail.com>
2026-01-07 17:43:30 +0530

    localhost port change

--- src/services/api.js
+++ src/services/api.js
@@ -1,17 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: "http://localhost:4000/api",
     headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
     },
 });
 
 // Interceptor to add token
 api.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
-        config.headers['x-auth-token'] = token;
+        config.headers["x-auth-token"] = token;
     }
     return config;
 });
